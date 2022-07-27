b324267f1ba0542c2e85cb0d63b0ee8a70d696b7
schillerandrew <schiller.andrew@gmail.com>
2022-07-26 22:39:01 -0700

    built README; added List component, with pagination, for displaying list of to-do items

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # todo-app
+
+> ## Problem Domain
+
+Refactor the To Do application:
+
+- Incorporate configuration settings to the application
+  - Add an editor so that users can save their preferences for the application, allowing them to change some of the default behaviors:
+    - how many items display on a page
+    - whether completed items are displayed
+
+> ## UML
+
+> ## Getting Started
+
+### Standard dependencies
+
+- react react-dom react-scripts sass uuid
+
+> ## Links
+
+- [GitHub pull request](https://github.com/schillerandrew/todo-app/pull/1)
+
+> ## Architecture (front-end)
+
+- library: React
+- styling: [Blueprint](https://blueprintjs.com/docs/#blueprint)
---
+++ src/components/List/index.js
@@ -0,0 +1,36 @@
+import { useState, useContext } from 'react';
+import { DisplayContext } from '../../context/Display';
+
+const List = ({ list, toggleComplete }) => {
+
+  const display = useContext(DisplayContext);
+  const [page, setPage] = useState(0);
+
+  const filteredList = list.filter(item => display.showCompleted ? true : !item.complete);
+  const start = display.itemsPerPage * page || 0;
+  const end = start + display.itemsPerPage || filteredList.length;
+  const pages = new Array(Math.ceil(filteredList.length / display.itemsPerPage)).fill('');
+
+  const displayList = filteredList ? filteredList.slice(start, end) : [];
+
+  return (
+    <>
+      {displayList.map(item => (
+        <div key={item.id}>
+          <p>{item.text}</p>
+          <p><small>Assigned to: {item.assignee}</small></p>
+          <p><small>Difficulty: {item.difficulty}</small></p>
+          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+          <hr />
+        </div>
+      ))}
+      {
+        pages.map((number, index) => (
+          <button key={`page-${index}`} onClick={() => setPage(index)}>{index + 1}</button>
+        ))
+      }
+    </>
+  )
+}
+
+export default List;
